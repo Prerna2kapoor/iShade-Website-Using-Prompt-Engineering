b17b43dd3b6e185d346496dfd00afa10dd4c1e9e
Prerna kapoor <162896351+Prerna2kapoor@users.noreply.github.com>
2025-02-02 01:03:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Cart modal functionality
+document.getElementById("addToCart").addEventListener("click", function() {
+    var cartModal = new bootstrap.Modal(document.getElementById("cartModal"));
+    cartModal.show();
+});
